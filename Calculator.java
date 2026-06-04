073b9e07b5c4c254285e7b3fb0db55dbd8216f39
2011ayushman-ctrl <2011.ayushman@gmail.com>
2026-06-04 10:48:37 -0500

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,19 @@
+class Calculator {
+    public static void main(String[] args) {
+ int a=15;
+ int b=20;
+int sum=a+b;
+int diff=a-b;
+int mul=a*b;
+int div=a/b;
+System.out.println("Addition of a&b is:"+sum);
+System.out.println("Subtraction of a&b is:"+diff);
+System.out.println("Multiplication of a&b is:"+mul);
+System.out.println("Division of a&b is:"+div);
+System.out.println("Addition of a&b:" + (a+b));
+System.out.println("Subtraction of a&b:" + (a-b));
+System.out.println("Multiplication of a&b:" + (a*b));
+System.out.println("Division of a&b:" + (a/b));
+System.out.println("Addition:" + (a+b) + "Subtraction:" + (a-b) + "Multiplication:" + (a*b) + "Division:" + (a/b));
+    }
+}
