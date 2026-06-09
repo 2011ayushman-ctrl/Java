6beeeb4115196b5052ab804e579275a668f0be38
2011ayushman-ctrl <2011.ayushman@gmail.com>
2026-06-09 09:42:56 -0500

    Add files via upload

---
+++ Loop.java
@@ -0,0 +1,17 @@
+public class Loop {
+    public static void main(String[] args) {
+        //using for loop
+        for (int i=7;i>2;i--){
+            System.out.println(i);
+        }
+        
+        //using while loop
+        int j = 8;
+        while (j<10){
+            System.out.println(j);
+            j++;
+        }
+        }
+}
+    
+
