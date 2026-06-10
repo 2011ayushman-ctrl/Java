06a3505b5ce37df54499a00dd71236289b4879ce
2011ayushman-ctrl <2011.ayushman@gmail.com>
2026-06-09 22:54:27 -0500

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,21 @@
+public class Reverse {
+    public static void main(String[] args) {
+        //students can even take manual inputs
+
+        int num = 54987, reversed = 0;
+
+        //run loop until num becomes 0
+        while(num !=0) {
+
+            //get last digit from num
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+
+            //remove the last digit from num
+            num /=10;
+        }
+
+        System.out.println("Reversed Number: " + reversed);
+
+        }
+    }
