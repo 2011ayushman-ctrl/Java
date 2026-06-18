de2b051cdda80a23abd808cbdf5e59d45f5bb7b0
2011ayushman-ctrl <2011.ayushman@gmail.com>
2026-06-18 10:31:10 -0500

    Add files via upload

---
+++ Parent1.java
@@ -0,0 +1,29 @@
+//this activity is mainly to understand the concept of inheritance
+//Parent Class
+class Parent
+{
+    int age, id;
+    String name;
+    void naming(String name)
+    {
+        System.out.println("Name:"+name);
+    }
+} 
+
+//child class
+class Child extends Parent
+{
+    void ageN(int age)
+    {
+        System.out.println("Age of student is:"+age);
+    }
+}
+class Parent1
+{
+    public static void main(String [] er)
+    {
+        Child s=new Child();//creating object of child class
+        s.naming("Aashish");
+        s.ageN(14);
+    }
+}
