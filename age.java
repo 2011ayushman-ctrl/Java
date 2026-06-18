862c177f9dc2960e520753d7e6321567c49cdab8
2011ayushman-ctrl <2011.ayushman@gmail.com>
2026-06-18 10:52:45 -0500

    Add files via upload

---
+++ age.java
@@ -0,0 +1,13 @@
+class age{
+
+    int age;
+    age(int age){
+        this.age = age;
+        //this.age = age;
+
+    }
+public static void main(String[] args){
+    age obj = new age(24);
+    System.out.println("obj.age =" + obj.age);
+}
+}
